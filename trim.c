d0cf575468ba55415bba974839f8fce1f71e3435
cyang <cy950812@gmail.com>
2017-07-17 14:58:57 +0800

    add trim

---
+++ trim.c
@@ -0,0 +1,61 @@
+/*
+* È¥³ý×Ö·û´®Ê×Î²¿Õ¸ñ
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+uint8_t *Trim(uint8_t *strVal)
+{
+	uint8_t *strptr2, *strptr3;
+
+	if(strVal == 0) 
+		return 0;
+	if(strlen((const char *)strVal) == 0) 
+		return strVal;
+
+	strptr2 = strVal;
+	while(strptr2 != 0 && *strptr2 == ' ')
+		strptr2++;
+
+	strptr3 = strptr2 + strlen((const char *)strptr2) -1;
+	while(*strptr3 == ' ' && strptr2 != strptr3)
+			strptr3--;
+	*(strptr3+1) = '0';
+
+	printf("strptr2:%s,strlen:%d\n",strptr2,strlen(strptr2));
+	printf("strptr3:%s,strlen:%d\n",strptr3,strlen(strptr3));
+	return strptr2;
+}
+
+
+void Trim2(uint8_t *str)
+{
+	int index=0,i=0;
+  
+	//È¥³ý´®Ê×¿Õ¸ñ 
+	while(str[index]==' ') index++;
+	for(i=0; i<strlen(str)-index; i++) str[i] = str[i+index]; 
+	str[i]='\0';
+  
+	//È¥³ý´®Î²¿Õ¸ñ 
+	index= strlen(str);
+	while(index>0 && str[index-1]==' ')  index--;
+	str[index]='\0';
+}
+
+int main()
+{
+	char str1[50] = "    hello world!    "; // test str
+	char *str2;
+
+	printf("str1:%s,strlen:%d\n",str1,strlen(str1));
+	//str2 = Trim(str1);
+	//printf("str2:%s,strlen:%d\n",str2,strlen(str2));
+
+	Trim2(str1);
+	printf("str1:%s,strlen:%d\n",str1,strlen(str1));
+
+	return 0;
+}
